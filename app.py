3ccebc1eb9fe919bb0dabc86350a2b0cd319ad32
crypto <ahmed.m4busines@gmail.com>
2021-09-21 16:12:20 -0700

    writing down the objectives of the app

---
+++ app.py
@@ -0,0 +1,4 @@
+# the main problem is receiving data from user and then use it as a visualization items,
+# and then need to delete them, will delete instead of saving data over time.
+# data will be csv and images
+# need to check streamlit documentation if i can do this other wise it will be a flask app.
