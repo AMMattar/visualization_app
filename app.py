6726e7c6763419f09b855accfc7333cd9505dbc7
crypto <ahmed.m4busines@gmail.com>
2021-09-22 17:37:43 -0700

    setting up

--- app.py
+++ app.py
@@ -2,3 +2,18 @@
 # and then need to delete them, will delete instead of saving data over time.
 # data will be csv and images
 # need to check streamlit documentation if i can do this other wise it will be a flask app.
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        return render_template('vis.html')
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
