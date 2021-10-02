1e4c1cce5d381cb1b1aa2a601e4e682d16c54650
crypto <ahmed.m4busines@gmail.com>
2021-10-02 05:42:54 -0700

    final touches

--- app.py
+++ app.py
@@ -2,13 +2,18 @@
 # and then need to delete them, will delete instead of saving data over time.
 # data will be csv and images
 # need to check streamlit documentation if i can do this other wise it will be a flask app.
-from flask import Flask, render_template, request, redirect
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import random
-import base64
 from io import BytesIO
+import io
+import PyQt5
+import base64
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from flask import Flask, render_template, request, redirect
+import matplotlib
+matplotlib.use('Agg')
+
 
 app = Flask(__name__)
 
@@ -58,18 +63,50 @@ def home():
                 numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad]+1
             total_reward = total_reward + 1
             ############## visualization #####################
-            new_plt = plt.figure()
+            '''
             plt.hist(ads_selected)
             plt.title('Histogram of ads selections')
             plt.xlabel('Ads')
             plt.ylabel('Number of times each ad was selected')
-            plt.show()
-            new_plt = plt.figure()
+            '''
             ######################################################
             assign = []
             for i in range(d):
                 x = numbers_of_rewards_0[i] + numbers_of_rewards_1[i]
                 assign.append(x)
+
+            to_sort_dict = {}
+            for i in range(d):
+                to_sort_dict[assign[i]] = work_images[i]
+
+            sorted_dict = sorted(to_sort_dict.items())
+            sorted_images = ""
+
+            def fig_to_base64(fig):
+                img = io.BytesIO()
+                # fig.save(img)
+                img.seek(0)
+                new_img = base64.b64encode(img.getvalue())
+                return new_img
+            for i in sorted_dict:
+                x = " \n "
+                # y = "<img src='data:image/png;base64,{}'>".format(
+                # fig_to_base64(i[1]).decode("utf-8"))
+                sorted_images = sorted_images + x
+            ##############################
+            tmpfile = BytesIO()
+            plt.savefig(tmpfile, format='png')
+            encoded = base64.b64encode(tmpfile.getvalue())
+
+            html_type = "<!DOCTYPE html >\n < html > \n <head> \n <meta charset='utf-8' />\n <title>visualization app</title>"
+            html_link = '''<link rel="stylesheet" type="text/css" href="{{ url_for('static',filename='css/vis_style.css') }}"/> \n </head> \n <body> '''
+            html_of_vis_of_plt = "<img src='data:image/png;base64,{}'>".format(
+                encoded.decode("utf-8"))
+            html_end = " </body > \n </html >"
+            full_file = html_type + html_link + html_of_vis_of_plt + sorted_images + html_end
+            with open('vis.html', 'w') as f:
+                f.write(full_file)
+                f.close()
             ##############
         return render_template('vis.html')
     else:
